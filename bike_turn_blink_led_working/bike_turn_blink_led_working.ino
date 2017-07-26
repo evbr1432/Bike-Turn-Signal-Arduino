f771afa0b7c2478802c11121165606670b8855f4
Evan Brookens <evbr1432@colorado.edu>
2017-07-25 20:33:54 -0600

    Finished comments and code. Waiting on Arduino Nano and single red LED's

--- bike_turn_blink_led_working/bike_turn_blink_led_working.ino
+++ bike_turn_blink_led_working/bike_turn_blink_led_working.ino
@@ -1,66 +1,88 @@
+//Evan Brookens
+//Arduino bike turn signal project
+//7/25/2017
 
-//Pins that the Led's and Buttons are connected too
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin1 =  13;      // the number of the LED pin
-const int ledPin2 =  6;      // the number of the LED pin
+//Hardware location on board:
+const int buttonPin = 2;     //Number of the pushbutton pin on the board
+const int ledPin1 =  13;     //Pin on the board of LED1
+const int ledPin2 =  6;      //Pin on the board of LED2
 
-//Variables
-int light_hold = 0;          //counter for keeping the light on (for loops etc...)
-int buttonState = 0;         // variable for reading the pushbutton status
-int count = 0;
+//Flash variables to custimize flash:
+int flash_count = 10;         //Number of times light flashes
+int flash_speed = 500;        //Milliseconds inbetween flashes
 
+//General Use Global Variables
+int buttonState = 0;         //Variable for reading the pushbutton status
+int button_hold_time = 0;    //Arbitrrary counter for length of time the button is held down
+int x = 0;                   //x value for flashing for loops (could be const)
+
+
+//Setup Values:
 void setup() {
-  // initialize the LED pin as an output:
+  //Initialize the LED pin as an output:
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  // initialize the pushbutton pin as an input:
+  //Initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  //Required for printing to serial monitor
   Serial.begin(9600);
 }
 
-//Main loop
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  // check if the pushbutton is pressed.
+  //Read the state of the button value:
+  //Check if the pushbutton is pressed.
   //LOW == Pressed 
+  buttonState = digitalRead(buttonPin);
   if (buttonState == LOW) {
-    //if pressed, add one to count and wait 400 miliseconds
-    count++;
-    delay(400);
+    //Call hold_time to check the amount of time the button was pressed
+    hold_time();
+    if (button_hold_time >= 2) {
+      //If button pressed and button_hold_time(arbitrary) added up to at least 2 (about 600 miliseconds).
+      for (x = 0; x < flash_count; x++) {
+        //Flash led pin 1 every ~flash_speed Miliseconds
+        digitalWrite(ledPin1, HIGH);
+        delay (flash_speed);
+        digitalWrite(ledPin1, LOW);
+        delay (flash_speed);     
+      }
+    }  
+    else {
+      for (x = 0; x < flash_count; x++) {
+        //Flash led pin 2 every ~flash_speed Miliseconds
+        digitalWrite(ledPin2, HIGH);
+        delay (flash_speed);
+        digitalWrite(ledPin2, LOW);
+        delay (flash_speed);
+      }
+    }
+    //Reset count
+    button_hold_time = 0;
   }
 
-  
-  if (buttonState == LOW) {
-    if (count >= 2) {
-      //if button pressed 
-      for (light_hold = 0; light_hold < 10; light_hold++){
-        digitalWrite(ledPin1, HIGH);
-        delay (500);
-        digitalWrite(ledPin1, LOW);
-        delay (500);
-        count = 0;
-      }
-      Serial.println("count loop 1:");
-      Serial.println(count);
-    }    
-    else{
-      for (light_hold = 0; light_hold < 10; light_hold++){
-        digitalWrite(ledPin2, HIGH);
-        delay (500);
-        digitalWrite(ledPin2, LOW);
-        delay (500);
-      }
-      Serial.println("count loop 2:");
-      Serial.println(count);
-    }
-  }
-  
-  else{
+  //If button is not pressed, turn off both LED's
+  else {
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin2, LOW);
   }
   
+}
+
+//Caluculates amount of time the button was pressed
+//Every iteration of while loop is ~300 miliseconds of button hold time
+
+int hold_time() {
+  while (buttonState == LOW) {
+    button_hold_time++;
+    delay(300);
+    //INFO for serial monitor to test correct return values.
+    Serial.print("Hold_time while counter: ");
+    Serial.print(button_hold_time);
+    Serial.print("\n");
+    //Read state so it can leave
+    buttonState = digitalRead(buttonPin);
+    }
+  //Return time the button was held while in this function
+  return button_hold_time;
 }
 
 
